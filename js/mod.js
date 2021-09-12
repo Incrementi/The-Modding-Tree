bae5136a33393bf59dfbab403b9122daa9b37ac1
Incrementi <90556892+Incrementi@users.noreply.github.com>
2021-09-12 19:52:57 +0800

    Update 1
    stuff in it

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Average Tree",
+	id: "incremod",
+	author: "Incrementi",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
